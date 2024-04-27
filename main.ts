7d7964e40a8c27e23199c68408b3dd52c36b094b
Bogdan Salyp <bogdansalyp@gmail.com>
2024-04-27 00:53:14 -0700

    feat: Added standard view

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, WorkspaceLeaf } from 'obsidian';
+import { ExampleView, VIEW_TYPE_EXAMPLE } from "./view";
 
 // Remember to rename these classes and interfaces!
 
@@ -16,9 +17,16 @@ export default class TutorPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
+		this.registerView(
+			VIEW_TYPE_EXAMPLE,
+			(leaf) => new ExampleView(leaf)
+		);
+
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
+			this.activateView();
+
 			const filePromises = this.app.vault.getMarkdownFiles().map(file => this.app.vault.cachedRead(file));
 			Promise.all(filePromises).then(texts => {
 				const questions = texts
@@ -79,6 +87,26 @@ export default class TutorPlugin extends Plugin {
 
 	onunload() {
 
+	}
+
+	async activateView() {
+		const { workspace } = this.app;
+	
+		let leaf: WorkspaceLeaf | null = null;
+		const leaves = workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE);
+	
+		if (leaves.length > 0) {
+		  // A leaf with our view already exists, use that
+		  leaf = leaves[0];
+		} else {
+		  // Our view could not be found in the workspace, create a new leaf
+		  // in the right sidebar for it
+		  leaf = workspace.getRightLeaf(false);
+		  await leaf?.setViewState({ type: VIEW_TYPE_EXAMPLE, active: true });
+		}
+
+		// "Reveal" the leaf in case it is in a collapsed sidebar
+		leaf && workspace.revealLeaf(leaf);
 	}
 
 	async loadSettings() {
